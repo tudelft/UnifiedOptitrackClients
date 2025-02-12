6186b9c1132602bd592588ac53834d243410f081
antbre <a.bredenbeck@tudelft.nl>
2025-02-12 20:07:39 +0100

    Clean-up compilation for ros2px4
    - sourcing script now checks for which ros 2 version is installed
    - no longer compiling the whole px4_msgs package but only the
      needed message.

--- scripts/source_ros_and_msgs.sh
+++ scripts/source_ros_and_msgs.sh
@@ -14,5 +14,22 @@
 # You should have received a copy of the GNU General Public License along
 # with this program. If not, see <https://www.gnu.org/licenses/>.
 
-source /opt/ros/humble/setup.bash
+# Check if ROS 2 is installed by checking for the existence of /opt/ros
+if [ -d /opt/ros ]; then
+    # Find the first directory under /opt/ros
+    ros_distro=$(ls /opt/ros | head -n 1)
+
+    # Source the setup.bash of the detected ROS 2 distro
+    if [ -n "$ros_distro" ]; then
+        source /opt/ros/$ros_distro/setup.bash
+        echo "Sourced ROS 2 $ros_distro setup."
+    else
+        echo "No ROS 2 distribution found under /opt/ros."
+        exit 1
+    fi
+else
+    echo "ROS 2 is not installed on this machine."
+    exit 1
+fi
+
 export LD_LIBRARY_PATH=../build/_deps/px4_msgs-build:${LD_LIBRARY_PATH}
